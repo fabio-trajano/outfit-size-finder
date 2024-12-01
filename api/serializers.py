cfbb6f2f798405595f41eadac249ca6fde30c59d
Fabio Trajano Ramalho <114175707+fabio-trajano@users.noreply.github.com>
2024-12-01 19:03:42 +0000

    fix serializers

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,6 @@ from rest_framework import serializers
 class SizePredictionSerializer(serializers.Serializer):
     height = serializers.FloatField()
     weight = serializers.FloatField()
-    preference = serializers.ChoiceField(choices=["tight", "regular" "loose"])
+    preference = serializers.ChoiceField(choices=["tight", "regular", "loose"])
     age = serializers.IntegerField()
     body_shape = serializers.ChoiceField(choices=["slim", "average", "athletic", "plus-size"])
--- machine_learning/data_generation.py
+++ machine_learning/data_generation.py
@@ -6,8 +6,8 @@ def generate_data(samples=1000):
     np.random.seed(42)
     data = {
         "height": np.random.normal(170, 10, samples),
-        "weight": np.random.normal(75, 15, samples),
-        "preference": np.random.choice(["tight", "regular", "loose"], samples),
+        "weight": np.random.normal(70, 15, samples),
+        "preference": np.random.choice(["tight", "loose", "regular"], samples),
         "age": np.random.randint(18, 60, samples),
         "body_shape": np.random.choice(["slim", "average", "athletic", "plus-size"], samples),
         "size": np.random.choice(["XXS", "XS", "S", "M", "L", "XL", "XXL"], samples)
